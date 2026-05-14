1a41a83dd7996ee633d1b9ebd5939177af804979
redmuschket <artemk306@gmail.com>
2026-05-14 16:41:23 +0500

    feat: add UUID generator utility

--- core/utils/generate_uuid.py
+++ core/utils/generate_uuid.py
@@ -1,6 +1,10 @@
 import uuid6
+from core import config
 
 
-def generate_uuid7():
-    """Generates UUID v7"""
-    return uuid6.uuid7()
+def generate_uuid_for_notification_from_config():
+    """Generates UUID v{N}"""
+    if config.NOTIFICATION_UUID_VERSION == 7:
+        return uuid6.uuid7()
+    else:
+        raise NotImplementedError(f"UUID version {config.NOTIFICATION_UUID_VERSION} is not supported")
