c1db0b2d9c2302770449d254b8a129204bd73a77
redmuschket <artemk306@gmail.com>
2026-05-14 17:27:46 +0500

    feat: timed decorator

---
+++ core/decorators/timed.py
@@ -0,0 +1,19 @@
+import time
+from core import logger
+from functools import wraps
+
+logger = logger.getLogger(__name__)
+
+
+def timed(func):
+    """
+    Decorator that logs the execution time of a route.
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        elapsed = time.perf_counter() - start
+        logger.info(f"{func.__name__} executed in {elapsed:.4f}s")
+        return result
+    return wrapper
