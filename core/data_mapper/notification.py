a07cbe5c9c8f1100c4d5ed8e5b608dc4cf6c0c2a
redmuschket <artemk306@gmail.com>
2026-05-14 17:57:51 +0500

    feat: data_mapper for notification

---
+++ core/data_mapper/notification.py
@@ -0,0 +1,25 @@
+from app.http.request_model.notification import NotificationCreateRequest
+from app.http.response_model.notification import NotificationCreateResponse
+from core.db.model.notification import NotificationModel
+from core.data_mapper.mapper import StaticMapper
+
+
+class NotificationMapper:
+
+    @staticmethod
+    def from_create_request_to_model(request: NotificationCreateRequest) -> NotificationModel:
+        """Pydantic create request -> SQLAlchemy model."""
+        return NotificationModel(
+            type=request.type,
+            recipient=request.recipient,
+            message=request.message,
+            subject=request.subject,
+        )
+
+    @staticmethod
+    def from_model_to_create_response(model: NotificationModel) -> NotificationCreateResponse:
+        """SQLAlchemy model -> Pydantic create response."""
+        return NotificationCreateResponse(
+            id=model.id,
+            status=model.status,
+        )
